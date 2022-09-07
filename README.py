d31ab09ebc5c873ee87db3847c7b540eb5759ca0
Kwame Doonquah <94137263+dkwame@users.noreply.github.com>
2022-09-06 22:50:10 -0400

    Create README.py

---
+++ README.py
@@ -0,0 +1,18 @@
+class dkwame:
+    def _init_(self):
+        self.username = 'dkwame'
+        self.name = 'Kwame Doonquah'
+        self.MSc = 'Computational Science & Engineering'
+        self.MSc_Uni = 'North Carolina A&T State University'
+        self.BA = 'Mathematics'
+        self.BA_Uni = 'University of North Carolina Wilmington'
+        self.linkedin = 'https://www.linkedin.com/in/kwame-doonquah-62779615a/'
+        self.email = 'doonkwa@gmail.com'
+        self.country = 'United States of America'
+
+
+    def _str_(self):
+        return self.name
+
+if _name_ == '_main_':
+    I_am = dkwame()
