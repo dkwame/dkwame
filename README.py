15f5b49efafab5bb20ebb50dc7222f22254473ef
Kwame Doonquah <94137263+dkwame@users.noreply.github.com>
2022-12-20 00:08:26 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,13 @@
 class dkwame:
     def _init_(self):
-        self.name = 'Kwame Doonquah'
+        self.Name = 'Kwame Doonquah'
         self.MSc_Uni = 'North Carolina A&T State University'
-        self.MSc_degree= 'Data Science & Engineering'
+        self.MSc_degree = 'Data Science & Engineering'
         self.BA_Uni = 'University of North Carolina Wilmington'
-        self.BA_degree= 'Mathematics'
-        self.linkedin = 'https://www.linkedin.com/in/kwame-doonquah-62779615a/'
-        self.email = 'doonkwa@gmail.com'
-        self.country = 'United States of America'
+        self.BA_degree = 'Mathematics'
+        self.Linkedin = 'https://www.linkedin.com/in/kwame-doonquah-62779615a/'
+        self.Email = 'doonkwa@gmail.com'
+        self.Country = 'United States of America'
 
 
     def _str_(self):
